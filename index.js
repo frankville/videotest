c181f2484ac24f5e644b75e96b997b3b62ba27f9
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-08-14 02:14:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+/*
+	$("#playBtn").click(function(event){
+		//event.pŕeventDefault();
+		playVideo();
+	});
+	$("#pauseBtn").click(function(event){
+		//event.pŕeventDefault();
+		pauseVideo();
+*/
+});
+
+
+
+function pauseVideo(){
+	document.getElementById("video1").pause();
+}
+
+function playVideo(){
+	document.getElementById("video1").play();
+}
